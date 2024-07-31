9b5d005498fefe04c02ba35158a937e100275eb7
Tetiana-V-Kovalenko <113469589+Tetiana-V-Kovalenko@users.noreply.github.com>
2024-07-31 17:04:00 +0300

    Initial commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export default function NotFoundPage() {
+  const { push, replace } = useRouter();
+  const pathname = usePathname();
+
+
+  return (
+    <div>
+      <div className="text-purple-600"> All not found, idiot</div>
+      <button className="p-2 bg-lime-300" onClick={() => push("/new")}>
+        Push
+      </button>
+      <button className="p-2 bg-red-300" onClick={() => replace("/new")}>
+        Replace
+      </button>
+    </div>
+  );
+}
---
+++ src/components/Button/CustomButton.tsx
@@ -0,0 +1,30 @@
+import { MouseEventHandler } from "react";
+
+type ButtonProps = {
+  title: string;
+  className?: string;
+  type?: "button" | "submit" | "reset" | undefined;
+  disabled?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+};
+
+const CustomButton = ({
+  title,
+  className,
+  type = "button",
+  disabled = false,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <button
+      disabled={disabled}
+      type={type}
+      className={`custom-btn  ${className}`}
+      onClick={onClick}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
